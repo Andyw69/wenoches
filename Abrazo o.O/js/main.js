abe8a88c9b112d8e0e45aa826abe9e3be1c11a19
Andyw69 <140619676+Andyw69@users.noreply.github.com>
2023-10-04 14:13:04 -0400

    Add files via upload

---
+++ Abrazo o.O/js/main.js
@@ -0,0 +1,18 @@
+setTimeout(cambiaMensaje, 2300)
+
+function cambiaMensaje(){    
+document.getElementById("loading-cargando").innerText = "Abrazo enviado!"
+}
+
+
+setTimeout(function(){
+    alert("Hey recibiste un Abacho :3")
+    alert("No me vas abrazar?👀🥺")
+    alert("Ush Obligado no vale 🤧! \nc va corriendo y c cae mas allasitos* xd sjsjjsjs")
+    document.getElementById("loading-message").innerText="tu sabias que los mamuts se extinguieron porque solo había hembras porque si hubiesen machos se llamarían paputs xd jajaja \ntu haces mi vida mas bonita :3"
+    document.getElementById("btn-btn-primarytm-intro-btn-tm-page-link").innerText="No tocar!"
+}, 3000)
+
+//setTimeout(function() {
+    
+//},) 
